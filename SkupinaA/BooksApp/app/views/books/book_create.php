9d68bf7e4996c292f35b0526f9e54891b9b8cb6a
Tonda Malý <tondamaly05@seznam.cz>
2026-03-22 21:06:00 +0100

    #5 PHP projekt - SkA - připojení k DB

--- SkupinaA/BooksApp/app/views/books/book_create.php
+++ SkupinaA/BooksApp/app/views/books/book_create.php
@@ -12,7 +12,7 @@
             <p>Vyplňte údaje a uložte kniho do databáze.</p>
         </div>
         <div>
-            <form action="">
+            <form action="../../controllers/BookController.php" method="post" enctype="multipart/form-data">
                 <div>
                     <div>
                         <label for="title">Název knihy <span>*</span></label>
@@ -20,11 +20,15 @@
                     </div>
                     <div>
                         <label for="author">Autor <span>*</span></label>
-                        <input type="text" id="author" name="author" required>
+                        <input type="text" id="author" name="author" placeholder="Příjmení Jméno" required>
                     </div>
                     <div>
-                        <label for="category">Kategorie </label>
-                        <input type="text" id="category" name="category">
+                        <label for="isbn">Rok vydání <span>*</span></label>
+                        <input type="number" id="year" name="year" required>
+                    </div>
+                    <div>
+                        <label for="category">ISBN <span>*</span></label>
+                        <input type="text" id="isbn" name="isbn">
                     </div>
                     <div>
                         <label for="subcategory">Podkategorie </label>
@@ -33,6 +37,26 @@
                     <div>
                         <label for="year">Rok vydání <span>*</span></label>
                         <input type="number" id="year" name="year" required>
+                    </div>
+                    <div>
+                        <label for="price">Cena knihy <span></span></label>
+                        <input type="number" id="price" name="price" step="0,5">
+                    </div>
+                    <div>
+                        <label for="link">Odkaz</label>
+                        <input type="number" id="link" name="link">
+                    </div>
+                    <div>
+                        <label for="description">Popis knihy </label>
+                        <textarea id="description" name="description" rows="5">Popis knihy: </textarea>
+                    </div>
+                    <div>
+                        <label>Obrázky (můžete nahrát více).</label>
+                        <label>
+                            <span>Klikni pro výběr souborů</span>
+                            <span>JPG / PNG / WebP – více souborů najednou</span>
+                            <input type="file" id="images" name="images[]" multiple accept="image/*" class="hidden">
+                        </label>
                     </div>
                     <div>
                         <button type="submit">Uložit knihu do DB</button>
