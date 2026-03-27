1b7673959d2a33f77d8f9e1102e6381751892caa
Tonda Malý <tondamaly05@seznam.cz>
2026-03-27 07:38:49 +0100

    #9 Ukládání - zobrazení formuláře z odkazu

--- SkupinaA/BooksApp/app/controllers/BookController.php
+++ SkupinaA/BooksApp/app/controllers/BookController.php
@@ -1,39 +1,21 @@
 <?php
-require_once '../app/models/Book.php';
 
 class BookController {
 
+    // 0. Výchozí metoda pro zobrazení úvodní stránky
     public function index() {
+        // V dalších krocích se zde přidá komunikace s Modelem pro získání dat z databáze
+        // (např. načtení všech uložených knih)
+        
+        // Nyní se pouze načte (vloží) připravený soubor s HTML strukturou
         require_once '../app/views/books/books_list.php';
     }
 
-    // Metoda pro zobrazení formuláře
+    // 1. Zobrazení formuláře pro přidání nové knihy
     public function create() {
+        // Zde se pouze načte (vloží) připravený soubor s HTML formulářem
         require_once '../app/views/books/book_create.php';
     }
 
-    public function store() {
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $bookModel = new Book();
-
-            // Vezmeme vše z formuláře (klíče musí odpovídat názvům v SQL nahoře)
-            $data = [
-                'title'       => $_POST['title'],
-                'author'      => $_POST['author'],
-                'category'    => $_POST['category'],
-                'subcategory' => $_POST['subcategory'],
-                'year'        => $_POST['year'],
-                'price'       => $_POST['price'],
-                'isbn'        => $_POST['isbn'],
-                'description' => $_POST['description'],
-                'link'        => $_POST['link']
-            ];
-
-            if ($bookModel->create($data)) {
-                // Po úspěchu přesměrujeme zpět na seznam
-                header('Location: index.php?url=book/index');
-                exit();
-            }
-        }
-    }
+    
 }
