fbef492c4d9f61340d761076960babe3b4ce0299
Tonda Malý <tondamaly05@seznam.cz>
2026-03-27 08:35:23 +0100

    #9 Ukládání knih - DONE na přednášce i s výpisem knih

--- SkupinaA/BooksApp/app/models/Book.php
+++ SkupinaA/BooksApp/app/models/Book.php
@@ -1,19 +1,51 @@
 <?php
-require_once 'Database.php';
 
 class Book {
-    private $db;
+    // Definice, že proměnná $db musí být vždy instancí třídy PDO
+    private PDO $db;
 
-    public function __construct() {
-        $database = new Database();
-        $this->db = $database->getConnection();
+    public function __construct(PDO $db) {
+        $this->db = $db;
     }
 
-    public function create($data) {
-        $sql = "INSERT INTO books (title, author, category, subcategory, year, price, isbn, description, link) 
-                VALUES (:title, :author, :category, :subcategory, :year, :price, :isbn, :description, :link)";
-        
+    public function create(
+        string $title,
+        string $author,
+        string $category,
+        string $subcategory,
+        int $year,
+        float $price,
+        string $isbn,
+        string $description,
+        string $link,
+        array $images
+    ): bool {
+        $sgl = "INSERT INTO books (title, author, category, subcategory, year, price, isbn, description, link, images)
+                VALUES (:title, :author, :category, :subcategory, :year, :price, :isbn, :description, :link, :images)";
+        // stmt = statement
         $stmt = $this->db->prepare($sql);
-        return $stmt->execute($data); 
+
+        return $stmt->execute([
+            ':title' => $title,
+            ':author' => $author,
+            ':category' => $category,
+            ':subcategory' => $subcategory ?: null,
+            ':year' => $year,
+            ':price' => $price,
+            ':isbn' => $isbn,
+            ':description' => $description,
+            ':link' => $link,
+            ':images' => json_encode($images)
+        ]);
+    }
+
+    // Získání všech knih z databáze
+    public function getAll() {
+        $sql = "SELECT * FROM books ORDER BY id DESC";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        
+        // Vrací pole asociativních polí (každý řádek z DB je jedno pole)
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
